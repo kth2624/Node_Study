14e39ec82ca1bb5db29e10b3974e59e50f048f79
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2020-08-25 22:51:13 +0900

    0825 study

---
+++ ExpressExample/10/config/config.js
@@ -0,0 +1,29 @@
+
+/*
+ * 설정
+ */
+
+module.exports = {
+	server_port: 3000,
+	db_url: 'mongodb://localhost:27017/local',
+	db_schemas: [
+	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
+	],
+	route_info: [
+	],
+	facebook: {		// passport facebook
+		clientID: '556196865058825',
+		clientSecret: 'e987481edb90f546abfc6cd4db7e9a93',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	},
+	twitter: {		// passport twitter
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {		// passport google
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/google/callback'
+	}
+}
