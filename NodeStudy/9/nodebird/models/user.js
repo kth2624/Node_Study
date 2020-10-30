21236aaf65a77e2d8ff0acb53089cccea661c0e7
Taeho <wpdldkdl77@naver.com>
2020-10-30 19:18:21 +0900

    1030 sns서비스 db셋팅

---
+++ NodeStudy/9/nodebird/models/user.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+//사용자 정보를 저장하는 모델 
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: true,
+        unique: true,
+      },
+      nick: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      provider: { //SNS로그인 했을때 provider가 local이면 로컬로그인, kakao면 카카오 로그인을 한 것.
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        defaultValue: 'local', //기본적으로 로컬 로그인이라 가정해서 defaultValue를 local로 주었다.
+      },
+      snsId: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followingId',
+      as: 'Followers',
+      through: 'Follow',
+    });
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followerId',
+      as: 'Followings',
+      through: 'Follow',
+    });
+  }
+};
